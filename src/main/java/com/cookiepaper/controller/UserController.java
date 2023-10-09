572ffc19c4ca226f06e565b46a693b293a02c1e3
heejin <dlagmlwls39@naver.com>
2023-10-09 16:02:11 +0900

    Fix: UserDto 추가 및 코드 수정

--- src/main/java/com/cookiepaper/controller/UserController.java
+++ src/main/java/com/cookiepaper/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.cookiepaper.controller;
 
-import com.cookiepaper.entity.User;
+import com.cookiepaper.dto.UserDto;
 import com.cookiepaper.service.MailService;
 import com.cookiepaper.service.MailServiceImpl;
 import com.cookiepaper.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user/*")
@@ -23,33 +22,28 @@ public class UserController {
 
     // 아이디 중복 확인
     @RequestMapping(value={ "checkId" }, method= RequestMethod.POST)
-    public @ResponseBody String checkId(String usId) throws Exception {
-        Long count = this.userService.checkId(usId);
-        return count <= 0 ? "ok" : "not ok";
+    public @ResponseBody ResponseEntity checkId(String usId) throws Exception {
+        Long count = userService.checkId(usId);
+        return count <= 0 ? new ResponseEntity<>("ok", HttpStatus.OK) : new ResponseEntity<>("not ok", HttpStatus.OK);
     }
 
     // 이메일 중복 확인
     @RequestMapping(value={ "checkEmail" }, method= RequestMethod.POST)
-    public @ResponseBody String checkEmail(String usEmail) throws Exception {
-        Long count = this.userService.checkEmail(usEmail);
+    public @ResponseBody ResponseEntity checkEmail(String usEmail) throws Exception {
+        Long count = userService.checkEmail(usEmail);
         if (count <= 0) {  // 사용 가능한 이메일인 경우 인증번호 발송
-            String authKey = this.mailService.sendMail(usEmail);
-            return authKey;
+            String authKey = mailService.sendMail(usEmail);
+            return new ResponseEntity<>(authKey, HttpStatus.OK);
         } else {  // 사용 불가능한 이메일인 경우
-            return "not ok";
+            return new ResponseEntity<>("not ok", HttpStatus.OK);
         }
     }
 
     // 회원가입
     @RequestMapping(value={ "signUp" }, method= RequestMethod.POST)
-    public @ResponseBody String signUp(User user) throws Exception {
-        User newUser = new User().builder()
-                        .usId(user.getUsId())
-                        .usPassword(user.getUsPassword())
-                        .usNickname(user.getUsNickname())
-                        .usEmail(user.getUsEmail())
-                        .build();
-        return this.userService.signUp(newUser) != null ? "success" : "fail";
+    public @ResponseBody ResponseEntity signUp(UserDto userDto) throws Exception {
+        return userService.signUp(userDto) != null ?
+                new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>("fail", HttpStatus.OK);
     }
 
 }
--- src/main/java/com/cookiepaper/service/UserServiceImpl.java
+++ src/main/java/com/cookiepaper/service/UserServiceImpl.java
@@ -1,41 +1,56 @@
 package com.cookiepaper.service;
 
+import com.cookiepaper.dto.UserDto;
 import com.cookiepaper.entity.User;
 import com.cookiepaper.repository.UserRepository;
+import com.cookiepaper.token.JwtTokenProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder bCryptPasswordEncoder) {
+    public UserServiceImpl(UserRepository userRepository,
+                           JwtTokenProvider jwtTokenProvider, PasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     // 아이디 중복 확인
     @Override
     public Long checkId(String usId) throws Exception {
-        Long userCnt = this.userRepository.countByUsId(usId);
+        Long userCnt = userRepository.countByUsId(usId);
         return userCnt;
     }
 
     // 이메일 중복 확인
     @Override
     public Long checkEmail(String usEmail) throws Exception {
-        Long userCnt = this.userRepository.countByUsEmail(usEmail);
+        Long userCnt = userRepository.countByUsEmail(usEmail);
         return userCnt;
     }
 
     // 회원가입
     @Override
-    public User signUp(User newUser) throws Exception {
-        newUser.hashPassword(bCryptPasswordEncoder);  // 비밀번호 암호화
+    @Transactional
+    public User signUp(UserDto userDto) throws Exception {
+        User newUser = User.builder()
+                .usId(userDto.getUsId())
+                .usPassword(bCryptPasswordEncoder.encode(userDto.getUsPassword()))
+                .usNickname(userDto.getUsNickname())
+                .usEmail(userDto.getUsEmail())
+                .build();  // 비밀번호 암호화
+
         System.out.println("newUser pw = " + newUser.getUsPassword());
-        return this.userRepository.save(newUser);
+        return userRepository.save(newUser);
     }
 
 }
