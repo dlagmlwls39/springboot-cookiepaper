fe8d33b71d23401217a520dfe385c1d67af4f221
heejin <dlagmlwls39@naver.com>
2023-10-29 02:28:28 +0900

    Fix: WebMvcConfig(CORS 설정) 수정

--- src/main/java/com/cookiepaper/config/WebMvcConfig.java
+++ src/main/java/com/cookiepaper/config/WebMvcConfig.java
@@ -2,9 +2,11 @@ package com.cookiepaper.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
@@ -13,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해
         registry.addMapping("/**")
-                // Origin이 http:localhost:3000에 대해
+                // Origin http:localhost:3000, http://15.165.55.131:80에 대해
                 .allowedOrigins("http://localhost:3000", "http://15.165.55.131:80")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드 허용
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
